fef180d888af5563fa879569152292924b6b098e
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-04-11 22:46:01 +0800

    微博

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -11,6 +11,11 @@ $GLOBALS['oauth_weixin'] = array(
     'appkey'		=>	'acef1288f8fa51a0df2df42a112f6c35',
     'callbackUrl'	=>	'http://test.iduotel.com/index/index/wxcallback',
 );
+$GLOBALS['oauth_weibo'] = array(
+    'appid'			=>	'2132894336',
+    'appkey'		=>	'0791cf9fdbb8bbb4f2622b4356690e63',
+    'callbackUrl'	=>	'http://test.iduotel.com/index/index/wbcallback',
+);
 
 class Index extends Controller
 {
@@ -62,5 +67,32 @@ class Index extends Controller
         log4()->debug($this->request->param());
     }
 
+    public  function weibo(){
+        $weiboOAuth = new \Yurun\OAuthLogin\Weibo\OAuth2($GLOBALS['oauth_weibo']['appid'], $GLOBALS['oauth_weibo']['appkey'], $GLOBALS['oauth_weibo']['callbackUrl']);
+// 解决只能设置一个回调域名的问题，下面地址需要改成你项目中的地址，可以参考./loginAgent.php写法
+// $weiboOAuth->loginAgentUrl = 'http://test.com/test/Weibo/loginAgent.php';
+// 可选属性
+        /*
+        // 授权页面的终端类型，取值见微博文档。http://open.weibo.com/wiki/Oauth2/authorize
+        $qqOAuth->display = null;
+        // 是否强制用户重新登录，true：是，false：否。默认false。
+        $qqOAuth->forcelogin = false;
+        // 授权页语言，缺省为中文简体版，en为英文版。
+        $qqOAuth->language = null;
+        */
+// 所有为null的可不传，这里为了演示和加注释就写了
+        $url = $weiboOAuth->getAuthUrl(
+            null,	// 回调地址，登录成功后返回该地址
+            null,	// state 为空自动生成
+            null	// scope 只要登录默认为空即可
+        );
+        $_SESSION['YURUN_WEIBO_STATE'] = $weiboOAuth->state;
+        header('location:' . $url);
+    }
+
+    public function wbcallback(){
+        log4()->debug("进入微博回调1");
+        log4()->debug($this->request->param());
+    }
 
 }
